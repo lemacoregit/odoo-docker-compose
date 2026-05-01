841e3ae7f87d71042de2f0c0aafd8f5d29995477
aldirrss <aldialputra@gmail.com>
2026-05-01 20:28:32 +0700

    feat: update version to 18.0.1.3.0 and add database connection parameters in demo registry

--- addons/18e/rpc-demo/demo_provisioner/__manifest__.py
+++ addons/18e/rpc-demo/demo_provisioner/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Demo Provisioner',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.3.0',
     'summary': 'Provisioning and management of custom Odoo module demo databases',
     'description': """
         Module for managing custom module demo environments.
